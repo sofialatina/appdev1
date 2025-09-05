e5956057a5f94a81729cecea43ca89a533707624
Sofia Latina <sofiaisabellatina@gmail.com>
2025-09-06 05:10:29 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,14 @@
+const greet = (name) => {
+    return `Hello, ${name}! <3`;
+}
+console.log(greet("Latina"));
+
+const square = (n) => {
+    return n * n;
+}
+console.log(square(4));
+
+const sayHi = () => {
+    console.log("Hi!");
+} 
+sayHi();
