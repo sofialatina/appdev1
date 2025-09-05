b980e268f9ea7b022b34dfffac622bf09aa4aa11
Sofia Latina <sofiaisabellatina@gmail.com>
2025-09-06 04:49:53 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variable.js
@@ -0,0 +1,18 @@
+let myName = "Latina";
+let age = 20;
+let isStudent = true;
+
+console.log("Name:", myName, "| Type:", typeof myName);
+console.log("Age:", age, "| Type:", typeof age);
+console.log("Is Student:", isStudent, "| Type:", typeof isStudent);
+
+let num0 = 7;
+let num1 = 4;
+
+console.log("Addition:", num0 + num1);
+console.log("Subtraction:", num0 - num1);
+console.log("Multiplication:", num0 * num1);
+console.log("Division:", num0 / num1);
+
+console.log(' "5" == 5:)', "5" == 5);
+console.log(' "5" === 5:)', "5" === 5);
