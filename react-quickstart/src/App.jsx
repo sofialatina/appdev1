0f9be44181c6f3ebb53794b488c9ffe94d062441
Sofia Latina <sofiaisabellatina@gmail.com>
2025-09-17 12:36:59 +0800

    Done displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -15,30 +15,22 @@ function App() {
     margin: 20,
   };
 
-  const quoteStyle = {
-    fontSize: '1.5em',
-    fontStyle: 'italic',
-    color: '#555',
-    textAlign: 'center',
-    maxWidth: '600px',
-    marginTop: '0',
-    padding: '0 20px',
+  const user = {
+    imageUrl: 'https://theculturednerd.org/wp-content/uploads/2020/04/Screenshot_20200417-145829_Chrome.jpg',
+    imageSize: '380px',
+    alignItems: 'center',
   };
 
   return (
     <div style={appStyle}>
+      <h1>{user.imageUrl}</h1>
       <img
-        style={imageStyle}
-        src="https://theculturednerd.org/wp-content/uploads/2020/04/Screenshot_20200417-145829_Chrome.jpg" 
-        alt="Studio Ghibli"
+        src={user.imageUrl}
+        alt="User"
+        style={{ ...imageStyle, width: user.imageSize, height: user.imageSize }}
       />
-      <blockquote style={quoteStyle}>
-        “Once you’ve met someone, you never really forget them.”
-        <br />
-        - Spirited Away 2001
-      </blockquote>
     </div>
-  );
+);
 }
 
 export default App;
