089e67137d78aa7bed3903372741ecd7e0767ddc
Sofia Latina <sofiaisabellatina@gmail.com>
2025-09-20 09:58:25 +0800

    Done rendering lists

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,36 +1,24 @@
-import react, { useState } from 'react';
-
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const products = [
+    { title: 'Potato', isFruit: false, id: 1 },
+    { title: 'Radish', isFruit: false, id: 2 },
+    { title: 'Mango', isFruit: true, id: 3 },
+  ];
 
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
-  const handleLogout= () => {
-    setIsLoggedIn(false);
-  };
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
 
   return (
-    <div className="App">
-      <h1>Welcome to Studio Ghibli's App!</h1>
-      {isLoggedIn ? (
-        <div>
-          <h2>Welcome back, User!</h2>
-          <img src="https://theculturednerd.org/wp-content/uploads/2020/04/Screenshot_20200417-145829_Chrome.jpg" alt="Logged In" />
-          <br></br>
-          <button onClick={handleLogout}>Log out</button>
-        </div>
-      ) : (
-        <div>
-          <h2>Please log in.</h2>
-          <img src="https://c4.wallpaperflare.com/wallpaper/764/209/100/movie-my-neighbor-totoro-mei-kusakabe-mini-totoro-my-neighbor-totoro-wallpaper-preview.jpg" alt="Logged Out" />
-          <br></br>
-          <button onClick={handleLogin}>Log in</button>
-        </div>
-      )}
-    </div>
-);
-}
+    <ul>{listItems}</ul>
+  );
+};
 
 export default App;
