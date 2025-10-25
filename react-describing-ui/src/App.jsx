b5ad1c44801d2b54eb16317cd3ef7b60bcbf0284
Sofia Latina <sofiaisabellatina@gmail.com>
2025-10-25 09:31:32 +0800

    Done with how to add markup to JavaScript with JSX

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,49 +1,17 @@
-function Profile() {
-  return (
-    <img
-src="https://i.imgur.com/QIrZWGIs.jpg"
-alt="Alan L. Hart"
-      style={{
-        width: "150px",
-        height: "150px",
-        margin: "10px",
-      }}
-    />
-  );
-}
-
-export default function Gallery() {
+export default function TodoList() {
 return (
-    <div
-      style={{
-        height: "100vh",           
-        width: "100vw",            
-        display: "flex",           
-        justifyContent: "center",  
-        alignItems: "center",      
-      }}
-    >
-      <section
-        style={{
-          textAlign: "center",
-          color: "white",
-        }}
-      >
-        <h1 style={{ marginBottom: "20px", fontSize: "2rem" }}>
-          Amazing Scientists
-        </h1>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            gap: "20px",
-          }}
-        >
-          <Profile />
-          <Profile />
-          <Profile />
-        </div>
-      </section>
-    </div>
-  );
+<>
+<h1>Hedy Lamarr's Todos</h1>
+<img
+src="https://i.imgur.com/yXOvdOSs.jpg"
+alt="Hedy Lamarr"
+className="photo"
+/>
+<ul>
+<li>Invent new traffic lights</li>
+<li>Rehearse a movie scene</li>
+<li>Improve spectrum technology</li>
+</ul>
+</>
+);
 }
