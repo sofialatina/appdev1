77845543d1a2406e4d1eeef851b613a6aef462e5
Sofia Latina <sofiaisabellatina@gmail.com>
2025-11-09 20:29:37 +0800

    Done with exporting and importing a component

--- react-importing-exporting-components/src/App.jsx
+++ react-importing-exporting-components/src/App.jsx
@@ -1,21 +1,8 @@
-import './App.css'
+import './App.css';
+import Gallery from './Gallery.jsx';
 
-function Profile() {
+export default function App() {
   return (
-    <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />
-  );
-}
-
-export default function Gallery() {
-  return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
+    <Gallery />
   );
 }
