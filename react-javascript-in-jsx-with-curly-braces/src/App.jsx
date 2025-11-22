a27cab28dbe98e18e60f41ef721af70dfd3c3eb4
Sofia Latina <sofiaisabellatina@gmail.com>
2025-11-22 14:33:06 +0800

    Done with using double curlies: CSS and other objects in JSX

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -4,8 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 export default function TodoList() {
-  const name = 'Gregorio Y. Zara';
   return (
-    <h1>{name}'s To Do List</h1>
+    <ul style={{
+      backgroundColor: 'black',
+      color: 'pink'
+    }}>
+      <li>Improve the videophone</li>
+      <li>Prepare aeronautics lectures</li>
+      <li>Work on the alcohol-fuelled engine</li>
+    </ul>
   );
 }
